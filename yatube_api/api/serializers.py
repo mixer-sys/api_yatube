b2d06139f2ae0cecb4f0696ee9cef2e51e64660e
Grigory <den.s1m@yandex.ru>
2023-06-26 22:39:37 +0300

    Fix issues

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -7,7 +7,7 @@ class PostSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Post
-        fields = ('id', 'text', 'author', 'image', 'group', 'pub_date')
+        fields = '__all__'
         read_only_fields = ('author',)
 
 
@@ -23,5 +23,5 @@ class CommentSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Comment
-        fields = ('id', 'author', 'post', 'text', 'created')
+        fields = '__all__'
         read_only_fields = ('author', 'post')
